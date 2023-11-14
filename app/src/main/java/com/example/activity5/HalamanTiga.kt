c2f9625b970ecfb831e519bd7bf7e368ba45ead1
ardianmulya <ardian.mulya7@gmail.com>
2023-11-14 19:20:27 +0700

    merubah nama function

--- app/src/main/java/com/example/activity5/HalamanTiga.kt
+++ app/src/main/java/com/example/activity5/HalamanTiga.kt
@@ -22,7 +22,7 @@ import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HalamanSatu(
+fun HalamanTiga(
     onSubmitClicked : (MutableList<String>)->Unit
 ) {
 
