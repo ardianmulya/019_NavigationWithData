c23900847a3869269a1ae03e8ad27d5e922e96dc
ardianmulya <ardian.mulya7@gmail.com>
2023-11-14 18:34:03 +0700

    menambah variabel nama noTelp alamat

--- app/src/main/java/com/example/activity5/data/OrderUiState.kt
+++ app/src/main/java/com/example/activity5/data/OrderUiState.kt
@@ -3,5 +3,8 @@ package com.example.activity5.data
 data class OrderUiState(
     val jumlah : Int = 0,
     val rasa : String = "",
-    val harga : String = ""
+    val harga : String = "",
+    val nama : String ="",
+    val noTelp : String = "",
+    val alamat : String = ""
 )
